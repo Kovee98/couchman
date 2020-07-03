be7eb077e3520db379232fe7650bd82da547b179
Joshua Kovalchik <kovee98@gmail.com>
2020-07-03 16:56:48 -0500

    Renamed connect => connection

--- src/components/Connection.vue
+++ src/components/Connection.vue
@@ -1,20 +1,13 @@
 <template>
     <div>
-        <b-button v-b-modal.connect variant="success" size="sm" class="float-left">
-            <b-icon-plus/>
-            Add Connection
-        </b-button>
-        <b-modal id="connect" ref="connect">
-            <template v-slot:modal-title>
-                Add Connection
-            </template>
+        <b-modal id="connection" :title="`${isNew ? 'Add' : 'Update'} Connection`">
             <div class="d-block">
                 <b-form>
                     <b-form-group
                         id="input-group-1"
                         label="Name"
                         label-for="input-1"
-                        description="Give this connection a meaningful name"
+                        description="Name this connection. Must be unique."
                     >
                         <b-form-input
                             id="input-1"
@@ -42,7 +35,6 @@
                         <b-form-input
                             id="input-3"
                             v-model="form.user"
-                            required
                         ></b-form-input>
                     </b-form-group>
 
@@ -50,7 +42,7 @@
                         <b-form-input
                             id="input-4"
                             v-model="form.pass"
-                            required
+                            type="password"
                         ></b-form-input>
                     </b-form-group>
                 </b-form>
@@ -60,9 +52,9 @@
                 <div>
                     <b-button
                         variant="success"
-                        @click="add"
+                        @click="submit"
                     >
-                        Add
+                        {{isNew ? 'Add' : 'Update'}}
                     </b-button>
                 </div>
             </template>
@@ -74,6 +66,7 @@
 export default {
     data: function () {
         return {
+            isNew: true,
             form: {
                 name: '',
                 url: '',
@@ -83,28 +76,62 @@ export default {
         };
     },
     methods: {
-        add: function (e) {
-            e.preventDefault();
+        submit: function () {
+            let conn = this.getConn();
 
+            let invalid = [];
+            if (!conn.name) invalid.push('name');
+            if (!conn.url) invalid.push('url');
+
+            if (invalid.length > 0) {
+                let fields = invalid.join(', ');
+                this.$events.$emit('alert-open', {
+                    variant: 'danger',
+                    msg: `Missing required fields: ${fields}`
+                });
+                return;
+            }
+
+            this.$events.$emit('alert-close');
+            this.$store.dispatch(`connections/${this.isNew ? 'add' : 'update'}`, conn);
+            this.$bvModal.hide('connection');
+        },
+        getConn: function (form) {
             let url = this.form.url;
             if (this.form.user && this.form.pass) {
                 url = url.split('//');
                 url = `${url[0]}//${this.form.user}:${this.form.pass}@${url[1]}`;
             }
 
-            let conn = {
+            return {
                 name: this.form.name,
                 url: url,
                 user: this.form.user,
                 pass: this.form.pass,
                 active: false
             };
-            debugger;
-
-            this.$store.dispatch('connections/add', conn);
-
-            this.$refs.connect.hide();
         }
+    },
+    created () {
+        this.$events.$on('connection-add', () => {
+            this.isNew = true;
+            this.form.name = '';
+            this.form.url = '';
+            this.form.user = '';
+            this.form.pass = '';
+
+            this.$bvModal.show('connection');
+        });
+
+        this.$events.$on('connection-edit', (conn) => {
+            this.isNew = false;
+            this.form.name = conn.name || '';
+            this.form.url = conn.url || '';
+            this.form.user = conn.user || '';
+            this.form.pass = conn.pass || '';
+
+            this.$bvModal.show('connection');
+        });
     }
 };
 </script>
