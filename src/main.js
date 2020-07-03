928458fb3aa07065ccfb61e59d630fdd6a2ae107
Joshua Kovalchik <kovee98@gmail.com>
2020-07-03 16:58:31 -0500

    Minor formatting

--- src/main.js
+++ src/main.js
@@ -2,17 +2,15 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import axios from 'axios';
 import events from './js/events';
+
+// bootstrap dependencies
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
-
-// Install BootstrapVue
 Vue.use(BootstrapVue);
-// Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin);
-
-import axios from 'axios';
 
 Vue.use({
     install (Vue) {
