085dbc6fc6b98dc01f106b58d97bebc27c87b224
Joshua Kovalchik <kovee98@gmail.com>
2020-07-03 16:54:47 -0500

    Added global event system.

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import events from './js/events';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
@@ -16,6 +17,7 @@ import axios from 'axios';
 Vue.use({
     install (Vue) {
         Vue.prototype.$axios = axios;
+        Vue.prototype.$events = events;
     }
 });
 
