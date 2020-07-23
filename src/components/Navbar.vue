c64745c55ccdf66e9739831c3cd9ce17d89c584e
Joshua Kovalchik <kovee98@gmail.com>
2020-07-23 07:27:29 -0500

    Removed unneeded navbar entries

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,16 +1,9 @@
 <template>
     <div>
         <b-navbar type="dark" variant="dark">
-            <b-navbar-brand href="/">Couchman</b-navbar-brand>
+            <b-navbar-brand href="/#/dbs">Couchman</b-navbar-brand>
             <b-navbar-nav>
-                <b-nav-item href="/#/dbs">Databases</b-nav-item>
                 <Connections/>
-
-                <!-- dropdown -->
-                <b-nav-item-dropdown text="Settings" right>
-                    <b-dropdown-item href="#">Account</b-dropdown-item>
-                    <b-dropdown-item href="#">Settings</b-dropdown-item>
-                </b-nav-item-dropdown>
             </b-navbar-nav>
         </b-navbar>
     </div>
