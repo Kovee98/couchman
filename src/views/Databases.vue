da55e43f3b1664fe9d67171bd420052e2e955e52
Joshua Kovalchik <kovee98@gmail.com>
2020-08-02 09:23:32 -0500

    Added pagination to dbs view

--- src/views/Databases.vue
+++ src/views/Databases.vue
@@ -6,40 +6,54 @@
             placeholder="Filter dbs..."
             class="my-3"
         />
-        <b-table
-            :items="dbs"
-            :fields="fields"
-            :filter="filter"
-            tbody-tr-class="tr"
-            head-variant="dark"
-            table-variant="light"
-            :busy="loading"
-            @row-clicked="open"
-            outlined
-            striped
-            fixed
-        >
-            <template v-slot:cell(actions)="row">
-                <span class="float-right">
-                    <div class="remove pl-2" @click.stop="remove(row)">
-                        <b-icon-trash font-scale="1.125" />
-                    </div>
-                </span>
-            </template>
-        </b-table>
+        <div class="mb-3" style="height: 550px">
+            <b-table
+                :items="dbs"
+                :fields="fields"
+                :filter="filter"
+                :per-page="perPage"
+                :current-page="currPage"
+                tbody-tr-class="tr"
+                head-variant="dark"
+                table-variant="light"
+                :busy="loading"
+                @row-clicked="open"
+                @filtered="updateNumPages"
+                outlined
+                striped
+                fixed
+            >
+                <template v-slot:cell(actions)="row">
+                    <span class="float-right">
+                        <div class="remove pl-2" @click.stop="remove(row)">
+                            <b-icon-trash font-scale="1.125" />
+                        </div>
+                    </span>
+                </template>
+            </b-table>
+        </div>
+
+        <Pagination :currPage="currPage" :numPages="numPages" />
     </div>
 </template>
 
 <script>
 import format from '../js/format';
+import Pagination from '@/components/Pagination';
 
 export default {
     name: 'Databases',
+    components: {
+        Pagination
+    },
     props: ['curr', 'conns'],
     data () {
         return {
             filter: '',
             dbs: [],
+            perPage: 10,
+            currPage: 1,
+            numPages: 0,
             fields: [
                 {
                     key: 'name',
@@ -84,6 +98,17 @@ export default {
         }
     },
     methods: {
+        updateNumPages (items = [], count = 0) {
+            let rem = count % this.perPage;
+            let numPages = Math.floor(count / this.perPage);
+            this.numPages = rem > 0 ? numPages + 1 : numPages;
+
+            if (this.currPage > this.numPages) {
+                this.currPage = this.numPages || 1;
+            } else if (this.currPage === 0) {
+                this.currPage = 1;
+            }
+        },
         open (db) {
             this.$router.push(`/dbs/${db.name}`);
         },
@@ -134,6 +159,9 @@ export default {
                                 doc_count: db.doc_count
                             };
                         });
+
+                        // update pagination
+                        this.updateNumPages(null, this.dbs.length);
                     }).catch((err) => {
                         this.$events.$emit('alert-open', {
                             variant: 'danger',
@@ -160,6 +188,10 @@ export default {
 
         this.$events.$on('refresh', (e) => {
             this.load();
+        });
+
+        this.$events.$on('set-curr-page', (currPage) => {
+            this.currPage = currPage;
         });
     }
 };
