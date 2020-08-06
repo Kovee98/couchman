318ee784160e7270d8e7438200e27f697608b1c0
Joshua Kovalchik <kovee98@gmail.com>
2020-08-06 07:23:56 -0500

    Added utils module with debounce and throttle

---
+++ src/js/utils.js
@@ -0,0 +1,40 @@
+function debounce (func, delay) {
+    let inDebounce;
+    return function () {
+        const context = this;
+        const args = arguments;
+        clearTimeout(inDebounce);
+        inDebounce = setTimeout(() => func.apply(context, args), delay);
+    };
+};
+
+function throttle (func, limit) {
+    let lastFunc;
+    let lastRan;
+    return function () {
+        const context = this;
+        const args = arguments;
+        if (!lastRan) {
+            func.apply(context, args);
+            lastRan = Date.now();
+        } else {
+            clearTimeout(lastFunc);
+            lastFunc = setTimeout(function () {
+                if ((Date.now() - lastRan) >= limit) {
+                    func.apply(context, args);
+                    lastRan = Date.now();
+                }
+            }, limit - (Date.now() - lastRan));
+        }
+    };
+};
+
+export {
+    debounce,
+    throttle
+};
+
+export default {
+    debounce,
+    throttle
+};
