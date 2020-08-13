75edc7a5da291c804ecc278897b7998724b0c1fd
Joshua Kovalchik <kovee98@gmail.com>
2020-08-13 07:31:17 -0500

    (feat) Added ability to toggle columns for views
    Why:
    - It's much simpler to create views this way.
    - It's a less error-prone way as well.

--- src/components/ViewModal.vue
+++ src/components/ViewModal.vue
@@ -32,41 +32,19 @@
                 />
             </b-form-group>
 
-            <b-form @submit.prevent="addCol">
-                <b-form-group
-                    id="view-cols-group"
-                    label="Columns"
-                    label-for="view-cols"
-                    description="Enter a column name and press enter"
+            <div>
+                <b-button
+                    v-for="btn in buttons"
+                    :key="btn.id"
+                    :pressed.sync="btn.state"
+                    size="sm"
+                    variant="outline-secondary"
+                    active-class="secondary"
+                    class="ml-2 mt-2"
                 >
-                    <b-form-input
-                        id="view-cols"
-                        v-model="col"
-                        type="search"
-                        placeholder="_id"
-                        required
-                        trim
-                    />
-                </b-form-group>
-                <div>
-                    <h4>
-                        <b-badge
-                            v-for="(col, i) in cols"
-                            :key="col.id"
-                            class="mr-2"
-                        >
-                            {{col}}
-                            <span class="clickable">
-                                <b-icon-trash
-                                    font-scale="0.8"
-                                    @click="removeCol(i)"
-                                    class="ml-2"
-                                />
-                            </span>
-                        </b-badge>
-                    </h4>
-                </div>
-            </b-form>
+                    {{btn.key}}
+                </b-button>
+            </div>
         </b-modal>
     </div>
 </template>
@@ -83,33 +61,45 @@ export default {
     data () {
         return {
             name: '',
-            col: '',
-            cols: ['age', 'name', 'animal']
+            buttons: []
         };
+    },
+    watch: {
+        fields () {
+            // TODO: remove 'actions' from selection - it'll always be last so remove the last field?
+            this.buttons = this.fields.map((field) => {
+                return {
+                    ...field,
+                    state: false
+                };
+            });
+        }
     },
     methods: {
         save () {
+            let cols = this.buttons.reduce((acc, curr) => {
+                if (curr.state) {
+                    acc.push(curr.key);
+                }
+                return acc;
+            }, []);
+
             this.$store.dispatch('views/add', {
                 view: {
                     name: this.name,
-                    cols: this.cols
+                    cols: cols
                 }
             });
         },
 
-        addCol () {
-            this.cols.push(this.col);
-            this.col = '';
-        },
-
-        removeCol (i) {
-            this.cols.splice(i, 1);
-        },
-
         reset () {
             this.name = '';
-            this.col = '';
-            this.cols = [];
+            this.buttons = this.fields.map((field) => {
+                return {
+                    ...field,
+                    state: false
+                };
+            });
         }
     }
 };
