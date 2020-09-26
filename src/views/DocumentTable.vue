7de96cdc1bbcd3e830793df69c12ddc994016a04
Joshua Kovalchik <kovee98@gmail.com>
2020-09-26 08:24:01 -0500

    refactor(dbs): move caches and isCacheReady into separate method

--- src/views/DocumentTable.vue
+++ src/views/DocumentTable.vue
@@ -192,15 +192,22 @@ export default {
                 'There are no fields that match the view'}`;
         },
 
+        isCacheReady () {
+            return this.$store.getters['cache/isReady'];
+        },
+
+        caches () {
+            return this.$store.getters['cache/caches'];
+        },
+
         docs () {
-            const caches = this.$store.getters['cache/caches'];
-            const isCacheReady = this.$store.getters['cache/isReady'];
             const currConn = this.currConn;
 
-            if (!isCacheReady || !currConn || (!currConn.id && currConn.id !== 0)) return [];
+            // return empty array until we have all the data necessary
+            if (!this.isCacheReady || !currConn || (!currConn.id && currConn.id !== 0)) return [];
 
             const db = this.$route.params.db;
-            const cache = caches[currConn.id][db];
+            const cache = this.caches[currConn.id][db];
 
             if (!cache) {
                 this.load();
